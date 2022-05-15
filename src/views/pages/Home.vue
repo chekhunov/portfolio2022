1e75c1a46629ef4a3ba9bdfc7f2f9a86a9ca5665
Ihor Chekhunov <ichekhunov.codica@gmail.com>
2022-05-16 00:22:19 +0300

    added section home and description

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,43 +1,63 @@
 <template>
-  <div class="home">
-    <div class="container">
-      <div class="home__inner d-flex justify-center align-center">
-        <div class="home__left">Igor Chekhunov</div>
+  <div class="home-page">
+    <section class="home">
+      <div class="container">
+        <div class="home__inner d-flex justify-center align-center">
+          <div class="home__left">Igor Chekhunov</div>
 
-        <div class="home__content">
-          <img
-            class="home__img"
-            src="../../root/assets/img/home1.png"
-            alt="photo"
-          />
+          <div class="home__content">
+            <img
+              class="home__img"
+              src="../../root/assets/img/home1.png"
+              alt="photo"
+            />
 
-          <div class="home__img-decoration-l d-flex flex-column">
-            <span v-for="(item, index) in 7" :key="`left_${index}`">
-              front-end
-            </span>
+            <div class="home__img-decoration-l d-flex flex-column">
+              <span v-for="(item, index) in 7" :key="`left_${index}`">
+                front-end
+              </span>
+            </div>
+
+            <div class="home__img-decoration-r d-flex flex-column">
+              <span v-for="(item, index) in 7" :key="`right_${index}`">
+                developer
+              </span>
+            </div>
           </div>
-          
-          <div class="home__img-decoration-r d-flex flex-column">
-            <span v-for="(item, index) in 7" :key="`right_${index}`">
-              developer
-            </span>
-          </div>
-        </div>
 
-        <div class="home__right">
-          <div class="title-block">
-            <img class="title-block__decor" src="../../root/assets/icons/circle.svg" alt="circle">
-            
-            <span class="title-block__text"> Start a project </span>
+          <div class="home__right">
+            <div class="title-block">
+              <img
+                class="title-block__decor"
+                src="../../root/assets/icons/circle.svg"
+                alt="circle"
+              />
+
+              <span class="title-block__text"> Start a project </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
+    <section class="description">
+      <div class="container">
+        <p class="description__headline">
+          I’m a <b style="color: #d06156">front-end developer</b> from kharkiv,
+          Ukraine
+        </p>
+
+        <div class="description__content">
+          <div class="description__text-l">
+            I build <b style="color: #d06156">creative</b>, eye-catching websites from scratch
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Home",
 };
